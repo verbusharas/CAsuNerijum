d5cfb3426e762a05abc58221cac26cb6c8b1e070
Sarunas Verbus <sarunas@verbus.lt>
2020-07-08 19:34:22 +0200

    added greet method calls with names

--- src/lt/verbus/Main.java
+++ src/lt/verbus/Main.java
@@ -3,7 +3,8 @@ package lt.verbus;
 public class Main {
 
     public static void main(String[] args) {
-
+        System.out.println(greet("Šarūnai"));
+        System.out.println(greet("Gitanai"));
     }
 
     public static String greet(String name) {
