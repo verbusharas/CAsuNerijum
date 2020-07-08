42896970d607e0ce3bcbe0d090211dde5e3551f3
Sarunas Verbus <sarunas@verbus.lt>
2020-07-08 19:26:32 +0200

    added greet method

--- src/lt/verbus/Main.java
+++ src/lt/verbus/Main.java
@@ -5,4 +5,8 @@ public class Main {
     public static void main(String[] args) {
 
     }
+
+    public static String greet(String name) {
+        return String.format("%s %s%s","Hello,", name, "!");
+    }
 }
